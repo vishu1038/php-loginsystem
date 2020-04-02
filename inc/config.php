127d6457609fab9d9f5876e2995da02120578ad7
Vishal Agarwal <agarwaldel20@gmail.com>
2020-04-02 11:51:08 +0530

    Added login/reg.php ; Added footer ; Added config

---
+++ inc/config.php
@@ -0,0 +1,5 @@
+<?php
+	if(!defined('__CONFIG__')) {
+		exit("You Do Not have a config file !");
+	}
+?>
---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+<!-- jQuery is required -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.3.7/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.3.7/js/uikit-icons.min.js"></script>
